a1a448dea8cc80888cc023b0fb4bf7d52bec3abd
OscarAg2 <oscaraguilarfut@gmail.com>
2024-02-29 16:21:15 -0400

    Creating the Search Functions

--- js/app.js
+++ js/app.js
@@ -2,13 +2,54 @@ const resultado = document.querySelector('#resultado');
 const year = document.querySelector('#year');
 const maxYear = new Date().getFullYear();
 const minYear = maxYear-10;
+const marca = document.querySelector("#marca");
+const precioMin = document.querySelector("#minimo");
+const precioMax = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+const datosBusqueda ={
+    marca:'',
+    year : '',
+    precioMin: '',
+    precioMax: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 document.addEventListener('DOMContentLoaded',()=>{
-    mostrarAutos();
+    mostrarAutos(autos);
     llenarYearSelect();
 })
 
-function mostrarAutos(){
+marca.addEventListener('change',e=>{
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+})
+
+function filtrarAuto(){
+    const resultado = autos.filter(filtrarMarca)
+    limpiarHTML();
+    mostrarAutos(resultado);
+    console.log(resultado);
+}
+
+function limpiarHTML(){
+    while (resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if (marca){
+        return auto.marca === marca;
+    } return auto;
+}
+
+function mostrarAutos(autos){
     autos.forEach(auto=>{
         const {marca,modelo,year,puertas,transmision,color,precio} = auto;
         const autoHTML = document.createElement('p');
